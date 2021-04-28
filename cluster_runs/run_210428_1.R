4268531d19075645ccde820b14a4ce4fd22ab310
Jon <test>
2021-04-28 12:54:19 +0200

    Prepared first cluster run...

--- cluster_runs/run_210428_1.R
+++ cluster_runs/run_210428_1.R
@@ -20,9 +20,14 @@ source("functions.R")
 
 num_cores <- detectCores()
 
-#mjmcmc_10K_res <- mclapply(1:20, function (x) {
-  mjmcmc(cbind(million_y_l, million_x)[1:10000,], logistic.loglik.aic, 655, sim_probs, sim_pars)
-#}, mc.cores = num_cores)
+data_10K <- cbind(million_y_l, million_x)[1:10000,]
+
+sim_probs <- gen.probs.list()
+sim_pars <- gen.params.list(data_10K)
+
+mjmcmc_10K_res <- mclapply(1:5, function (x) {
+  mjmcmc(data_10K, logistic.loglik.aic, 655, sim_probs, sim_pars)
+}, mc.cores = num_cores)
 
 save(mjmcmc_10K_res, file="mjmcmc_10K_res.Rdata")
 ftpUpload("mjmcmc_10K_res.Rdata", paste0("ftp://jon:",args[1],"@jonlachmann.se/mjmcmc_10K_res.Rdata"))
