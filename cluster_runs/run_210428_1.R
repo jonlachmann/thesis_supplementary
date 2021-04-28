593a7a405d501cd24517cce7ac495d5305232b61
Jon <test>
2021-04-28 12:46:13 +0200

    Prepared first cluster run...

--- cluster_runs/run_210428_1.R
+++ cluster_runs/run_210428_1.R
@@ -7,19 +7,19 @@
 args <- commandArgs(trailingOnly=TRUE)
 
 # Load packages
-source("../packages.R")
+source("packages.R")
 # Simulate data
-source("../sim_data1.R")
+source("sim_data1.R")
 # Load likelihood functions
-source("../likelihoods1.R")
+source("likelihoods1.R")
 # Load common functions
-source("../functions.R")
+source("functions.R")
 
 num_cores <- detectCores()
 
-#mjmcmc_10K_res <- mclapply(1:20, function (x) {
+mjmcmc_10K_res <- mclapply(1:20, function (x) {
   mjmcmc(cbind(million_y_l, million_x)[1:10000,], logistic.loglik.aic, 655, sim_probs, sim_pars)
-#}, mc.cores = num_cores)
+}, mc.cores = num_cores)
 
 save(mjmcmc_10K_res, file="mjmcmc_10K_res.Rdata")
 ftpUpload("mjmcmc_10K_res.Rdata", paste0("ftp://jon:",args[1],"@jonlachmann.se/mjmcmc_10K_res.Rdata"))
