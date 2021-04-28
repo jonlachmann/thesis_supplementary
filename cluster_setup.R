20466e289cd97179a3b76983058bf22dd7af2819
Jon <test>
2021-04-28 12:40:10 +0200

    Prepared first cluster run...

--- cluster_setup.R
+++ cluster_setup.R
@@ -7,12 +7,12 @@
 .libPaths("/cluster/home/jola4668/R")
 
 # Load devtools
-install.packages("usethis", repos='http://cran.us.r-project.org')
-install.packages("devtools", repos='http://cran.us.r-project.org')
+#install.packages("usethis", repos='http://cran.us.r-project.org')
+#install.packages("devtools", repos='http://cran.us.r-project.org')
 library(devtools)
 
 # Load RCurl
-install.packages("RCurl", repos='http://cran.us.r-project.org')
+#install.packages("RCurl", repos='http://cran.us.r-project.org')
 library(RCurl)
 
 # Install our packages
