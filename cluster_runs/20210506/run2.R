28165bd5b07586cfeaeb882b99b08ba8f1ba2c65
Jon <test>
2021-05-06 15:08:01 +0200

    A lot of things... Run prepared

--- cluster_runs/20210506/run2.R
+++ cluster_runs/20210506/run2.R
@@ -10,7 +10,7 @@ if (length(args)!=1) {
 
 # Load packages, data, likelihood functions and general functions
 source("packages.R")
-source("gauss_sim_data.R")
+source("logis_sim_data.R")
 source("likelihoods1.R")
 source("functions.R")
 
@@ -21,13 +21,13 @@ set.seed(Sys.time())
 n_obs <- 100000
 dirname <- create_randdir()
 set.seed(as.numeric(dirname)+Sys.time())
-basename <- paste0("run",dirname, "_mjmcmc_100Kg")
+basename <- paste0("run",dirname, "_mjmcmc_100Kl")
 sub_size <- c(0.05,0.01,0.0075,0.005,0.0025,0.001,0.0005)
 subs <- sub_size[as.numeric(args[1])]
 run_count <- 20
 iter <- 32768
 
-data_use <- cbind(mill_y_g100K, mill_x_g)[1:n_obs,]
+data_use <- cbind(mill_y_l100K, mill_x_g)[1:n_obs,]
 
 sim_probs <- gen.probs.list()
 sim_pars <- gen.params.list(data_use)
@@ -35,4 +35,4 @@ sim_pars$loglik$g <- 100/sqrt(n_obs/100)
 sim_probs$large <- 0.05
 
 print(paste0("Running ",subs,"run!"))
-run_mjmcmc(linear.g.prior.loglik.irlssgd, data_use, sim_probs, sim_pars, subs, iter, run_count, basename)
+run_mjmcmc(logistic.loglik.bic.irlssgd, data_use, sim_probs, sim_pars, subs, iter, run_count, basename)
