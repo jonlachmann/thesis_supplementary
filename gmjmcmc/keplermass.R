a5ca776a4c0ab2d78377aabeab4d462ee0835ada
Jon <test>
2021-05-09 17:53:25 +0200

    Some kepler results analysis

--- gmjmcmc/keplermass.R
+++ gmjmcmc/keplermass.R
@@ -4,12 +4,42 @@
 # Created on: 2021-05-03
 
 source("packages.R")
+source("functions.R")
 library(GMJMCMC)
 
 kmdata <- as.matrix(read.csv("data/gmjmcmc/exa1.csv"))
 kmdata <- cbind(kmdata[,5], kmdata[,-5])
 
-transforms <- c("cosi","sigmoid","tanh","atan","sini","troot")
+transforms <- c("sigmoid","sini","tanh","atan","troot")
+set.transforms(transforms)
+
+# Load results from disk
+kepler_dir <- "data/gmjmcmc/kepler/"
+kepler_runs <- vector("list")
+kepler_files <- list.files(path=paste0(kepler_dir))
+for (i in 1:length(kepler_files)) kepler_runs[[i]] <- loadRdata(file=paste0(kepler_dir,kepler_files[i]))
+
+kepler100 <- merge.results(kepler_runs, 2, 0.01)
+
+resplot(kepler100, 10)
+
+resmat <- matrix(NA,100,1)
+for(i in 1:length(kepler_runs)) resmat[i,] <- kepler_runs[[i]]$best
+
+sapply(kepler_runs[[80]]$populations[[40]], print)
+
+mods <- matrix(unlist(kepler_runs[[80]]$models[[i]]), ncol=length(kepler_runs[[80]]$models[[i]][[1]]$model)+3, byrow=T)
+
+for(i in 1:40) {
+  print(max(matrix(unlist(kepler_runs[[80]]$models[[i]]), ncol=length(kepler_runs[[80]]$models[[i]][[1]]$model)+3, byrow=T)[,length(kepler_runs[[80]]$models[[i]][[1]]$model)+2]))
+  print(i)
+}
+cbind(sapply(kepler_runs[[80]]$populations[[28]], print), t(unlist(GMJMCMC:::marginal.probs.renorm(kepler_runs[[80]]$models[[28]]))))
+
+lab <- sapply(kepler_runs[[80]]$populations[[26]], print)
+marg <- unlist(GMJMCMC:::marginal.probs.renorm(kepler_runs[[80]]$models[[26]]))
+
+cbind(lab, t(marg))
 
 km_pars <- gen.params.list(kmdata, T)
 km_probs <- gen.probs.list(transforms)
@@ -30,7 +60,7 @@ pargmj <- mclapply(1:8, function (x) {
 
 set.transforms(transforms)
 
-mergeres <- merge.results(pargmj)
+mergeres <- merge.results(pargmj, 2)
 resplot(mergeres, 10)
 
 resplot <- function (result, count) {
