22e66cdd2a498f676ecd134cafaddc275305b2c1
Jon <test>
2021-05-12 23:36:31 +0200

    Prepared for running!

---
+++ cluster_runs/20210512/run2.R
@@ -0,0 +1,44 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: jonlachmann
+# Created on: 2021-05-10
+
+.libPaths("/cluster/home/jola4668/R")
+
+n_obs <- 1000
+
+source("packages.R")
+source("likelihoods1.R")
+source("blr/sim_blr.R")
+source("functions.R")
+
+dirname <- create_randdir()
+
+set.seed(as.numeric(dirname)+Sys.time())
+basename <- paste0("run",dirname, "_gmjmcmc_blr")
+run_count <- 20
+subs <- 1
+
+transforms <- c("sigmoid","sini","tanh","atan","troot")
+
+blrdata <- cbind(blr_y, blr_x)
+
+blr_pars <- gen.params.list(blrdata, T)
+blr_probs <- gen.probs.list(transforms)
+
+blr_probs$filter <- 0.9
+blr_pars$loglik$r <- 1/nrow(blrdata)
+blr_pars$loglik$subs <- subs
+blr_pars$feat$pop.max <- 40
+blr_pars$feat$prel.filter <- 0.9
+blr_pars$feat$keep.org <- T
+blr_pars$feat$keep.min <- 0.8
+blr_pars$feat$D <- 5
+blr_probs$gen <- c(10, 5, 1, 1)
+blr_pars$rescale.large <- T
+
+num_cores <- detectCores()
+
+print(paste0("Running ",basename,"run!"))
+run_gmjmcmc(run_count, basename, subs,
+            blrdata, gaussian.loglik.aic, gaussian.loglik.alpha, transforms, 60, 500, 2000, blr_probs, blr_pars, F)
